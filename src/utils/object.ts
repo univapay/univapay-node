98e6210b87b8f3c6e6b81a75f8f2d6feac9078f7
Leong Wai Yin <668915+laxa88@users.noreply.github.com>
2020-06-10 16:39:41 +0900

    fix transformKeys to also parse parent as array

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -7,8 +7,13 @@ import { Omit } from 'type-zoo';
 export type Transformer = (...args: any[]) => string;
 
 export function transformKeys(obj: any, transformer: Transformer, ignoreKeys: string[] = []): any {
+    const c: (o: any) => boolean = (o: any): boolean => typeof o === 'object' && Boolean(o);
+
+    if (Array.isArray(obj)) {
+        return (obj as any[]).map((i: any) => (c(i) ? transformKeys(i, transformer) : i));
+    }
+
     return Object.keys(obj || {}).reduce((r: any, k: string) => {
-        const c: (o: any) => boolean = (o: any): boolean => typeof o === 'object' && Boolean(o);
         let v: any = (obj as any)[k];
 
         if (ignoreKeys.includes(k)) {
