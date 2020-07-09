010434cc0a5f8a1be14f15d04c786dcd60c41a99
Pierre Hieronymus <p.hieronymus@univapaycast.com>
2020-07-09 11:05:33 +0900

    Add corporation business types

---
+++ src/resources/Corporation.ts
@@ -0,0 +1,8 @@
+export enum businessTypes {
+    PRODUCT_SALES = "product_sales",
+    CONSULTING = "consulting",
+    TRAVEL = "travel",
+    DIGITAL_SALES = "digital_sales",
+    AESTHETIC_SALON = "aesthetic_salon",
+    OTHER = "other",
+}
