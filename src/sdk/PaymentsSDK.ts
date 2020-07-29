70c1229d502344ebbca9c63d8933c3e347f8073e
Julian Grinblat <julian@dotcore.co.il>
2020-07-29 12:41:37 +0900

    Support event emitters from PaymentsSDK and Resource

--- src/sdk/PaymentsSDK.ts
+++ src/sdk/PaymentsSDK.ts
@@ -2,12 +2,18 @@
  *  @module SDK
  */
 
+import { EventEmitter } from "events";
+
 import { RestAPI, RestAPIOptions } from "../api/RestAPI";
 
-export abstract class PaymentsSDK {
+export abstract class PaymentsSDK extends EventEmitter {
     api: RestAPI;
 
     constructor(options?: RestAPIOptions) {
+        super();
+
         this.api = new RestAPI(options);
+        this.on("newListener", (event, listener) => this.api.on(event, listener));
+        this.on("removeListener", (event, listener) => this.api.removeListener(event, listener));
     }
 }
