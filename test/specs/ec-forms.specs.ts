f140c158f4864bb44d7d6adad478f12c68a9ff99
Pierre Hieronymus <p.hieronymus@univapaycast.com>
2020-12-01 15:35:29 +0900

    Add missing ec-form tests

---
+++ test/specs/ec-forms.specs.ts
@@ -0,0 +1,38 @@
+import { expect } from "chai";
+import fetchMock from "fetch-mock";
+import { v4 as uuid } from "uuid";
+
+import { RestAPI } from "../../src/api/RestAPI";
+import { ECForms } from "../../src/resources/ECForms";
+import { generateFixture as generateCheckoutInfo } from "../fixtures/checkout-info";
+import { testEndpoint } from "../utils";
+import { pathToRegexMatcher } from "../utils/routes";
+
+describe("EC Forms", () => {
+    let api: RestAPI;
+    let ecForms: ECForms;
+
+    const recordData = generateCheckoutInfo();
+    const recordPathMatcher = pathToRegexMatcher(`${testEndpoint}/forms/:id`);
+
+    beforeEach(() => {
+        api = new RestAPI({ endpoint: testEndpoint });
+        ecForms = new ECForms(api);
+    });
+
+    afterEach(() => {
+        fetchMock.restore();
+    });
+
+    context("GET /forms/:id", () => {
+        it("should get response", async () => {
+            fetchMock.getOnce(recordPathMatcher, {
+                status: 200,
+                body: recordData,
+                headers: { "Content-Type": "application/json" },
+            });
+
+            await expect(ecForms.get(uuid())).to.eventually.eql(recordData);
+        });
+    });
+});
