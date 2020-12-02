109dde30592b4dfa84a7dc7f4efa028c86af7ec3
Pierre Hieronymus <p.hieronymus@univapaycast.com>
2020-12-02 14:21:05 +0900

    Add hack to handle errors serialize issue

--- src/errors/RequestResponseError.ts
+++ src/errors/RequestResponseError.ts
@@ -7,12 +7,13 @@ import { ErrorResponse } from "../api/RestAPI";
 type ErrorRequest = Omit<ErrorResponse, "status">;
 
 const serializeErrorResponse = ({ code, httpCode, errors }: ErrorRequest): string => {
-    const formattedErrors = (errors || []).filter(Boolean).map((error) => {
+    // (pH) FIXME: `errors sometimes is not an array. Fix type and maybe the cause of it
+    const formattedErrors = (errors || [])?.filter(Boolean).map((error) => {
         switch (typeof error) {
             case "string":
             case "number":
             case "boolean":
-                return error; // (pH) TODO: special handling for wrongly formatted. Fix the ErrorRequest type.
+                return error; // (pH) FIXME: special handling for wrongly formatted. Fix the ErrorRequest type.
 
             default:
                 return "field" in error ? `${error.reason} (${error.field})` : `${error.reason}`;
