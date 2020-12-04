dc35eb187a677d7d9dfc71f2d11d5bede51de4c4
Hieronymus Pierre <54787068+phieronymus@users.noreply.github.com>
2020-12-04 16:55:31 +0900

    Add none array errors handling (#98)

--- src/errors/RequestResponseError.ts
+++ src/errors/RequestResponseError.ts
@@ -9,15 +9,22 @@ type ErrorRequest = Omit<ErrorResponse, "status">;
 const isSymbol = (value: unknown): value is symbol => ["string", "number", "boolean"].includes(typeof value);
 const serializeErrorResponse = ({ code, httpCode, errors }: ErrorRequest): string => {
     // (pH) FIXME: `errors sometimes is not an array. Fix type and maybe the cause of it
-    const formattedErrors = isSymbol(errors)
-        ? errors
-        : (errors || []).filter(Boolean).map((error: SubError | ValidationError | symbol) => {
-              if (isSymbol(error)) {
-                  return error; // (pH) FIXME: special handling for wrongly formatted. Fix the ErrorRequest type.
-              }
+    const formattedErrors = (() => {
+        if (!errors || isSymbol(errors)) {
+            return errors;
+        }
 
-              return "field" in error ? `${error.reason} (${error.field})` : `${error.reason}`;
-          });
+        // (pH) FIXME: Remove this once the ts services will be fixed
+        const errorList = Array.isArray(errors) ? errors : [errors];
+
+        return errorList.filter(Boolean).map((error: SubError | ValidationError | symbol) => {
+            if (isSymbol(error)) {
+                return error; // (pH) FIXME: special handling for wrongly formatted. Fix the ErrorRequest type.
+            }
+
+            return "field" in error ? `${error.reason} (${error.field})` : `${error.reason}`;
+        });
+    })();
 
     return `Code: ${code}, HttpCode: ${httpCode}, Errors: ${formattedErrors.join(", ")}`;
 };
