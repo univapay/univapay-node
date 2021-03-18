1ea401a2ebd6d926da88e1a97bd83af9b8241991
Pierre Hieronymus <p.hieronymus@univapaycast.com>
2021-03-18 14:16:47 +0900

    Add tagged credentials gateway error

--- src/errors/APIError.ts
+++ src/errors/APIError.ts
@@ -221,6 +221,7 @@ export enum ResponseErrorCode {
 
     /* Gateway simulation */
     PlatformCredentialsDisabled = "PLATFORM_CREDENTIALS_DISABLED",
+    TaggedPlatformCredentialsDisabled = "TAGGED_PLATFORM_CREDENTIAL_DISABLED",
     ForbiddenGateway = "FORBIDDEN_GATEWAY",
     ForbiddenQrGateway = "FORBIDDEN_QR_GATEWAY",
     BlacklistedTagOnMerchant = "BLACKLISTED_TAG_ON_MERCHANT",
