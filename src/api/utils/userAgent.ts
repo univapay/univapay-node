a47d0c9a100f7427e5ffe24ec3222757933e4fa2
Julian Grinblat <julian@dotcore.co.il>
2022-06-01 12:53:19 +0900

    Add user-agent header

---
+++ src/api/utils/userAgent.ts
@@ -0,0 +1,26 @@
+import { parse } from "bowser";
+import { isBrowser, isNode } from "browser-or-node";
+
+const CLIENT_NAME = "univapay-node";
+
+function getPlatform() {
+    if (isNode) {
+       return `node.js@${process.versions.node}`;
+    }
+
+    if (isBrowser) {
+        const { browser: { name, version } } = parse(window.navigator.userAgent);
+
+        return `${name}@${version}`;
+    }
+
+    return "uknown-platform"
+}
+
+function getModuleType() {
+    return typeof require === 'function' ? 'commonjs' : 'module';
+}
+
+export function userAgent() {
+    return  `${CLIENT_NAME} ${getPlatform()} ${getModuleType()}`;
+}
