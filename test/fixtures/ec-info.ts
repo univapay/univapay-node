d805600d0774c4eecb7a3eb811e0edbf4a974fd6
Julian Grinblat <julian@dotcore.co.il>
2022-06-07 17:22:05 +0900

    Fix

--- test/fixtures/ec-info.ts
+++ test/fixtures/ec-info.ts
@@ -1,4 +1,4 @@
-import { ECInfoItem } from "../../src/resources";
+import { ECInfoItem } from "../../src/resources/index.js";
 
 import { generateFixture as generateForm } from "./ec-form.js";
 import { generateFixture as generateLink } from "./ec-form-link.js";
