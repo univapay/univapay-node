70f1253d2835fe05f8de40d46ed11d1d24249299
Pierre Hieronymus <p.hieronymus@univapaycast.com>
2023-02-14 13:33:12 +0900

    Fix transform keys type

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -20,20 +20,17 @@ export const isBlob = (data: unknown): data is Blob =>
     typeof data.type === "string" &&
     typeof data.slice === "function";
 
-export const transformKeys = (
-    obj: Record<string, any> | unknown[],
+export const transformKeys = <Data = Record<string, unknown> | unknown[]>(
+    obj: Data,
     transformer: Transformer,
     ignoreKeys: string[] = []
-): any => {
-    const transformArray = (arr: unknown[]): any =>
-        arr.map((item) => (isObject(item) ? transformKeys(item, transformer, ignoreKeys) : item));
-
+): Data => {
     if (Array.isArray(obj)) {
-        return transformArray(obj);
+        return obj.map((item) => (isObject(item) ? transformKeys(item, transformer, ignoreKeys) : item)) as Data;
     }
 
     const ignoredKeySet = new Set(ignoreKeys);
-    return Object.keys(obj || {}).reduce((acc: Record<string, any>, key: string) => {
+    return Object.keys(obj || {}).reduce((acc: Data, key: string) => {
         const value = obj[key];
 
         if (ignoredKeySet.has(key)) {
@@ -45,5 +42,5 @@ export const transformKeys = (
 
         acc[transformer(key)] = formattedValue;
         return acc;
-    }, {});
+    }, {}) as Data;
 };
