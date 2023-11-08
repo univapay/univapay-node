daed4557ad795bd7544ea868778d6c334082142d
Pierre Hieronymus <p.hieronymus@univapaycast.com>
2023-11-08 17:48:29 +0900

    Fix subscription simulation required parameters

--- src/resources/Subscriptions.ts
+++ src/resources/Subscriptions.ts
@@ -311,7 +311,7 @@ const hasImmediateCharge = (subscription: SubscriptionItem) => {
 
 export class Subscriptions extends CRUDResource {
     static requiredParams = ["transactionTokenId", "amount", "currency", ["period", "cyclicalPeriod"]];
-    static requiredSimulationParams: string[] = ["installmentPlan", "paymentType", "currency", "period"];
+    static requiredSimulationParams: string[] = ["paymentType", "currency", "period"];
 
     static routeBase = "/stores/:storeId/subscriptions";
 
