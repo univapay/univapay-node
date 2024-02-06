28e8302c2cf573ca97435fccabb324fafcc99aa5
Julian Grinblat <julian@dotcore.co.il>
2024-02-06 18:07:09 +0900

    feat(polling): add environment parameters for polling

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,14 +1,23 @@
+import { env } from "node:process";
+
 /**
  *  @internal
  *  @module Constants
  */
+
+const parseInt = (str: string | undefined): number | undefined => {
+    if (str == undefined) return undefined;
+
+    const parsed = Number.parseInt(str);
+    return Number.isNaN(parsed) ? undefined : parsed;
+};
 
 export const DEFAULT_ENDPOINT = "https://api.univapay.com";
 export const ENV_KEY_ENDPOINT = "UNIVAPAY_ENDPOINT";
 export const ENV_KEY_APP_ID = "UNIVAPAY_APP_ID";
 export const ENV_KEY_APPLICATION_JWT = "UNIVAPAY_APPLICATION_JWT";
 export const ENV_KEY_SECRET = "UNIVAPAY_SECRET";
-export const POLLING_TIMEOUT = 600000; // 10 minutes
-export const POLLING_INTERVAL = 1000; // 1 second
+export const POLLING_TIMEOUT = parseInt(env?.POLLING_TIMEOUT) ?? 600000; // 10 minutes
+export const POLLING_INTERVAL = parseInt(env?.POLLING_INTERVAL) ?? 1000; // 1 second
 export const IDEMPOTENCY_KEY_HEADER = "Idempotency-Key";
 export const MAX_INTERNAL_ERROR_RETRY = 3;
