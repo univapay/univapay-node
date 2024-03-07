b1dbf53252694f65d4a3694d45bceea8e110ce80
Pierre Hieronymus <p.hieronymus@univapay.com>
2024-03-07 16:52:10 +0900

    Add more granular customer roles

--- src/resources/common/Configuration.ts
+++ src/resources/common/Configuration.ts
@@ -18,10 +18,13 @@ export enum TransferMatchAmount {
 export enum CustomerRole {
     TRANSACTION_TOKEN_READ = "transaction_token_read",
     TRANSACTION_TOKEN_UPDATE = "transaction_token_update",
-    SUBSCRIPTION_READ = "subscription_read",
+    TRANSACTION_TOKEN_CREATE = "subscription_delete",
+    DISPLAY_PAYMENT_METHOD = "display_payment_method",
+
+    SUBSCRIPTION_READ = "subscription_read", // Also controls subscription page display on the management system
     SUBSCRIPTION_UPDATE_TOKEN = "subscription_update_token",
     SUBSCRIPTION_SUSPEND = "subscription_suspend", // Also controls resume functionality
-    SUBSCRIPTION_DELETE = "subscription_delete",
+    SUBSCRIPTION_DELETE = "transaction_token_create",
 }
 
 export type CustomerManagementConfiguration = {
