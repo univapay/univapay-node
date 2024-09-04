8aa07162bf1393442257cbb15cbbe70c44da6fd8
Pierre Hieronymus <p.hieronymus@univapay.com>
2024-09-04 17:46:31 +0900

    Fix error key

--- src/errors/APIError.ts
+++ src/errors/APIError.ts
@@ -283,7 +283,7 @@ export enum ResponseErrorCode {
     MustContainPlatformCurrency = "MUST_CONTAIN_PLATFORM_CURRENCY",
     CurrencyMustBeInAmountsList = "CURRENCY_MUST_BE_IN_AMOUNTS_LIST",
     NumberMin = "NUMBER_MIN",
-    UnfinishedChargeExistForToken = "UNFINISHED_CHARGE_EXIST_FOR_TOKEN",
+    UnfinishedChargeExistForToken = "UNFINISHED_CHARGES_EXIST_FOR_TOKEN",
 
     BrandNotDefined = "BRAND_NOT_DEFINED",
 
