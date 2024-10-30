06c1eab0595dc9348077da1fd6fa62ddc42c0a60
gdp88 <d.georgios@univapay.com>
2024-10-30 17:26:45 +0900

    Add 'rateLimit' function to utils/fetch (#1196)

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -5,9 +5,48 @@
 
 import { parse } from "@apimatic/json-bigint";
 
-import { APIError } from "../errors/APIError.js";
+import { APIError, ResponseErrorCode } from "../errors/APIError.js";
+import { ResponseError } from "../errors/RequestResponseError.js";
 
 import { toCamelCase, transformKeys } from "./object.js";
+
+const COOLDOWN_ADJUST = 1.2;
+const COOLDOWN_WAIT = 50; // Cooldown wait period after first fail (50ms)
+const FAIL_TRESHOLD = 10; // Fail processing after X concurrent fails. If 0, then will try forever.
+
+export const rateLimit = <R>(exec: (...args: any[]) => Promise<R>): Promise<R> => {
+    let cooldown = 0;
+    let tries = 0;
+
+    return new Promise((resolve, reject) => {
+        const executor = async () => {
+            try {
+                tries++;
+                const result: R = await exec();
+                cooldown = 0;
+                tries = 0;
+                resolve(result);
+            } catch (error) {
+                if (
+                    error instanceof ResponseError &&
+                    (error.errorResponse.code === ResponseErrorCode.TooManyRequests ||
+                        error.errorResponse.httpCode === 429)
+                ) {
+                    if (FAIL_TRESHOLD > 0 && tries > FAIL_TRESHOLD) {
+                        reject(error);
+                    } else {
+                        cooldown = cooldown === 0 ? COOLDOWN_WAIT : cooldown * COOLDOWN_ADJUST;
+                        setTimeout(executor, cooldown);
+                    }
+                } else {
+                    reject(error);
+                }
+            }
+        };
+
+        return executor();
+    });
+};
 
 export const parseJSON = async <FormattedBody>(
     response: Response,
